77a0fa9cd0c0cb785301a94500457cda0c06f497
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-02-09 10:21:33 +0630

    Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import "./App.css";
-import Greet from "./components/Greet";
-import Heading from "./components/Heading";
-import Oscar from "./components/Oscar";
-import Status from "./components/Status";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div className="App">
-      <Status status="error" />
-      <Heading>Placeholder Text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicpario!</Heading>
-      </Oscar>
-      <Greet name="GG" isLoggedIn={true} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,9 @@
+type ContainerProps = {
+  styles: React.CSSProperties;
+};
+
+function Container(props: ContainerProps) {
+  return <div style={props.styles}>Text content goes here</div>;
+}
+
+export default Container;
