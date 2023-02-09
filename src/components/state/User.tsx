d9a060087060b687d0c5bb31c494160051cfbc2c
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-02-09 14:33:31 +0630

    useState Type Assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -5,7 +5,7 @@ type AuthUser = {
 };
 
 function User() {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
 
   const handleLogin = () => {
     setUser({
@@ -13,16 +13,12 @@ function User() {
       email: "email",
     });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
 
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email}</div>
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
     </div>
   );
 }
