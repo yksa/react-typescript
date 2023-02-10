e61600c4ffe0194ef416ab3cceab755c42c34939
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-02-10 15:36:14 +0630

    class component

---
+++ src/components/class/Counter.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+type CounterProps = {
+  message: string;
+};
+
+type CounterState = {
+  count: number;
+};
+
+// The count value is 5
+export default class Counter extends Component<CounterProps, CounterState> {
+  state = { count: 0 };
+
+  handleClick = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Increment</button>
+        {this.props.message} {this.state.count}
+      </div>
+    );
+  }
+}
