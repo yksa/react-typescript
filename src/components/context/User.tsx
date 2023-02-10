31336e7101a66fde25352265f7643d77414ae997
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-02-10 14:21:32 +0630

    useContext Future Value

---
+++ src/components/context/User.tsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+
+function User() {
+  const userContext = useContext(UserContext);
+  const handleLogin = () => {
+    userContext.setUser({ name: "gg", email: "email.@g" });
+  };
+  const handleLogout = () => {
+    userContext.setUser(null);
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <div>User name is {userContext.user?.name}</div>
+      <div>User email is {userContext.user?.email}</div>
+    </div>
+  );
+}
+
+export default User;
