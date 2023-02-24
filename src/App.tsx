efe70ea43d60719420e1564ccb1482ebe412f8e7
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-02-24 14:53:24 +0630

    Polymorphic Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
-import { CustomButton } from "./components/html/Button";
+import Text from "./components/polymorphic/Text";
 
 function App() {
   return (
     <div className="App">
-      <CustomButton variant="primary" onClick={() => console.log("clicked")}>
-        Primary Button
-      </CustomButton>
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor="someId" size="sm" color="secondary">
+        Label
+      </Text>
     </div>
   );
 }
