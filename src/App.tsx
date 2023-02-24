5b439266be17dbcbb3a35eb9ceedb8bd393c91a4
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-02-24 10:19:22 +0630

    Wrapping HTML elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Toast from "./components/templateliterals/Toast";
+import { CustomButton } from "./components/html/Button";
 
 function App() {
   return (
     <div className="App">
-      <Toast position="center" />
+      <CustomButton variant="primary" onClick={() => console.log("clicked")}>
+        Primary Button
+      </CustomButton>
     </div>
   );
 }
